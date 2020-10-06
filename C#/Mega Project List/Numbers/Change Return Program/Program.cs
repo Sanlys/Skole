6ef1a43a6afe2d9088d1bcf2cb4e348dfc0c8f7b
0xHannah <71554254+0xHannah@users.noreply.github.com>
2020-10-06 12:48:52 +0200

    Moved folder with C# Projects into repo

---
+++ C#/Mega Project List/Numbers/Change Return Program/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Change_Return_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] amounts = { 0, 0, 0, 0, 0 };
+            double[] values = { 1, 0.25, 0.1, 0.05, 0.01 };
+
+            double val;
+            double cost;
+
+            Console.WriteLine("Enter cost");
+            cost = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter payment");
+            val = Convert.ToDouble(Console.ReadLine()) - cost;
+
+            for (int x = 0; amounts.Length <= x; x++)
+            {
+                while (val % values[x] == 0 && val != 0)
+                {
+                    val -= values[x];
+                    amounts[x] += values[x];
+                }
+            }
+            
+            foreach (int i in amounts)
+            {
+                Console.WriteLine("Amounts: " + i.ToString());
+                Console.WriteLine("Val: " + val);
+            }
+        }
+    }
+}
---
+++ C#/Mega Project List/Numbers/Prime Factorization/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Prime_Factorization
+{
+    class Program
+    {
+
+        public static void factorization(int x)
+        {
+            int y = 2;
+            while (y < 10)
+            {
+                if (x % y == 0)
+                {
+                    Console.Write(y + " * ");
+                    x /= y;
+                }
+                y++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int value = Convert.ToInt32(Console.ReadLine());
+            factorization(value);
+        }
+    }
+}
