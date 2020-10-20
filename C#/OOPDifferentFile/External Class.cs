eb57d0d5b095c904f0da46c263d699523e57629c
Sander <Sander@DESKTOP-S3CB981>
2020-10-20 15:18:55 +0200

    .

---
+++ C#/OOPDifferentFile/External Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPDifferentFile
+{
+    class External_Class
+    {
+        public int x;
+    }
+}
---
+++ C#/OOPDifferentFile/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOPDifferentFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            External_Class a = new External_Class();
+            a.x = 10;
+            Console.WriteLine(a.x);
+        }
+    }
+}
