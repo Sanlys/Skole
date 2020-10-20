4b976a2c0a642f2cfe0441c2e711d1210e481f5d
Sander <Sander@DESKTOP-S3CB981>
2020-10-20 15:24:19 +0200

    .

--- C#/OOPDifferentFile/Program.cs
+++ C#/OOPDifferentFile/Program.cs
@@ -6,9 +6,10 @@ namespace OOPDifferentFile
     {
         static void Main(string[] args)
         {
-            External_Class a = new External_Class();
+            External_Class a = new External_Class("Text");
             a.x = 10;
             Console.WriteLine(a.x);
+            Console.WriteLine(a.y);
         }
     }
 }
